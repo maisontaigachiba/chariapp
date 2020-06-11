473068139754bce299c15b32b2def2a68842f746
Taiga Chiba <timsofy1@gmail.com>
2020-06-11 20:13:28 +0900

    /usersの構築

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '7a18f66acb83c0918982399a6d0ab25aadaafaf15990b11b508e041759144f902e2be54177590f027f80bb74c7e90bcde1e60931be7f1580a1b84bdf1f1224f2'
