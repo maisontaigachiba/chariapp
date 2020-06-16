3b63ffd65be970f7b8221e67b6fb1c80aefb7e5a
Taiga Chiba <timsofy1@gmail.com>
2020-06-16 14:36:57 +0900

    ログインせずに自転車一覧を閲覧できるように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,15 @@ class ApplicationController < ActionController::Base
   before_action :configue_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resouce)
-    users_path
+    bikes_path
   end
 
   def after_sign_up_path_for(resouce)
-    users_path
+    bikes_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
-    new_user_session_path
+    root_path
   end
 
 
