76a75c2f8845ada7e345fb1eb4ca5877e5bb4335
Taiga Chiba <timsofy1@gmail.com>
2020-06-19 08:42:34 +0900

    404page 500pageの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configue_permitted_parameters, if: :devise_controller?
-
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  rescue_from ActionController::RoutingError, with: :render_404
+  rescue_from Exception, with: :render_500
+  
+  def render_404
+    render template: 'errors/error_404', status: 404, layout: 'application', content_type: 'text/html'
+  end
+  
+  def render_500
+    render template: 'errors/error_500', status: 500, layout: 'application', content_type: 'text/html'
+  end  
   def after_sign_in_path_for(resouce)
     bikes_path
   end
@@ -12,7 +22,6 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource_or_scope)
     root_path
   end
-
 
   protected
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "home#index"
+  devise_for :users
+  get "/about" => "home#about"
+  get "/contact" => "home#contact"
   resources :users
   resources :bikes do
     resource :favorites, only: [:create, :destroy]
   end
+  get '*path', controller: 'application', action: 'render_404'
 end
