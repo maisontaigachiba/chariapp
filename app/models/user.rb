ae9809154375e8cd2d3a5051865f5d01a06eac2b
Taiga Chiba <timsofy1@gmail.com>
2020-06-22 12:06:48 +0900

    フラッシュメッセージの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   validates :username,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
-  validates :password, presence: true, length: { minimum: 6 }
-  validates :password_confirmation, presence: true
+  validates :password, presence: true, length: { minimum: 6 }, on: :update, allow_blank: true #ユーザー編集画面でパスワードの変更を無視するためのバリデーション
+  validates :password_confirmation, presence: true, on: :update, allow_blank: true
   attachment :profile_image
   has_many :bikes, dependent: :destroy
   has_many :favorites, dependent: :destroy
